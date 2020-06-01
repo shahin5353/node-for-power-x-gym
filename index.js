f46e02edce285c4fd881f9fbf2cbbdcf174a54e9
Shahin <ahmshahin5353@gmail.com>
2020-06-01 17:40:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = process.env.DB_PATH
+let client = new MongoClient(uri, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/addClasses', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    const classes = req.body
+    client.connect(err => {
+        const collection = client.db("power-x-gym").collection("classes")
+        collection.insert(classes, ( err, result ) =>{
+            if(err){
+                console.log(err);
+                res.status(500).send({message : err})
+            }
+            else {
+                res.send(result.ops)
+            }
+        })
+        client.close()
+    })
+    
+})
+
+app.get('/classes', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    client.connect(err =>{
+        const collection = client.db("power-x-gym").collection("classes")
+        collection.find().toArray((err, documents) =>{
+            if(err) {
+                console.log(err);
+                res.status(500).send({ message : err })                
+            }
+            else{
+                res.send(documents)
+            }
+        })
+        client.close()
+    })
+})
+
+app.post('/addPricing', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    const pricing = req.body
+    client.connect(err => {
+        const collection = client.db("power-x-gym").collection("pricing_plans")
+        collection.insert(pricing, ( err, result ) =>{
+            if(err){
+                console.log(err);
+                res.status(500).send({message : err})
+            }
+            else {
+                res.send(result.ops)
+            }
+        })
+        client.close()
+    })
+    
+})
+
+app.get('/pricing-plans', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    client.connect(err =>{
+        const collection = client.db("power-x-gym").collection("pricing_plans")
+        collection.find().toArray((err, documents) =>{
+            if(err) {
+                console.log(err);
+                res.status(500).send({ message : err })                
+            }
+            else{
+                res.send(documents)
+            }
+        })
+        client.close()
+    })
+})
+
+app.post('/addUser', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    const user = req.body
+    client.connect(err => {
+        const collection = client.db("power-x-gym").collection("users")
+        collection.insertOne(user, ( err, result ) =>{
+            if(err){
+                console.log(err);
+                res.status(500).send({message : err})
+            }
+            else {
+                res.send(result.ops[0])
+            }
+        })
+        client.close()
+    })
+    
+})
+
+
+app.listen(process.env.PORT, () => console.log(`Listening from port ${process.env.PORT}`))
